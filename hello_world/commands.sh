e10ecc9c5574b613f671477f00a08b840c9586a5
RNDr. Šimon Tóth <happy.cerberus@gmail.com>
2021-08-30 16:40:26 +0000

    Hello World example

---
+++ hello_world/commands.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Pre-process non-modular hello_world.cc with clang++
+echo "Size of pre-processed non-modular hello world in bytes:"
+clang++ -std=c++20 -stdlib=libc++ -E hello_world.cc | wc -c
+echo ""
+
+# Pre-process modular hello_modular_world.cc with clang++
+echo "Size of pre-processed modular hello world in bytes:"
+clang++ -std=c++20 -stdlib=libc++ -fmodules -fbuiltin-module-map -E hello_modular_world.cc | wc -c
+echo ""
+
+# Non-modular compile time
+echo "Compiling non-modular hello world:"
+time clang++ -std=c++20 -stdlib=libc++ hello_world.cc -o hello_world
+./hello_world
+echo ""
+
+echo "Compiling modular hello world:"
+time clang++ -std=c++20 -stdlib=libc++ -fmodules -fbuiltin-module-map hello_modular_world.cc -o hello_modular_world
+./hello_modular_world
+echo ""
---
+++ hello_world/hello_modular_world.cc
@@ -0,0 +1,5 @@
+import <iostream>;
+
+int main() {
+	std::cout << "Hello Modular World!\n";
+}
